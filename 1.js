80e01ab88fd7eaf96a377132054e624004f13144
Ji seungmin <jinow9595@gmail.com>
2022-02-09 00:12:42 +0900

    let, const - object

---
+++ 1.js
@@ -0,0 +1,30 @@
+
+let now = new Date();
+const height = 188;
+
+// console.log(now);
+// alert(now);
+
+// console.log(null === "");
+// console.log(undefined === "");
+// console.log(null === undefined);
+// console.log(null === null);
+
+const array = ["1", 2, 32, "fas"];
+
+array.push("last");
+
+console.log(array);
+
+const me = {
+    lastName: "ji",
+    firstName: "now"
+}
+
+console.log(me);
+console.log(me.lastName);
+console.log(me["firstName"]);
+
+me.blog = "jinow9595.tistory.com";
+
+console.log(me);
