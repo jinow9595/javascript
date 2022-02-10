ce16f9dbc01cdf628d33a9397e7f1b6db86fbf83
Ji seungmin <jinow9595@gmail.com>
2022-02-10 21:25:54 +0900

    const object - event listener

--- 1.js
+++ 1.js
@@ -28,3 +28,41 @@ console.log(me["firstName"]);
 me.blog = "jinow9595.tistory.com";
 
 console.log(me);
+
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+const title = document.querySelector("#title");
+
+const superEventHandler = {
+    mouseenter: function (index) {
+        title.style.color = colors[index];
+        title.innerText = "The mouse is here!";
+    },
+    mouseleave: function (index) {
+        title.style.color = colors[index];
+        title.innerText = "The mouse is gone!";
+    },
+    contextmenu: function (index) {
+        title.style.color = colors[index];
+        title.innerText = "That was a right click!";
+    },
+    resize: function (index) {
+        title.style.color = colors[index];
+        title.innerText = "You just resized!";
+    }
+};
+
+title.addEventListener("mouseenter", function () {
+    superEventHandler.mouseenter(0);
+});
+
+title.addEventListener("mouseleave", function () {
+    superEventHandler.mouseleave(1);
+});
+
+window.addEventListener("contextmenu", function () {
+    superEventHandler.contextmenu(4);
+});
+
+window.addEventListener("resize", function () {
+    superEventHandler.resize(2);
+});
