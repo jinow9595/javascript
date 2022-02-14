76e1ae062eb536c75eb1ccec801d509d41581a6f
Ji seungmin <jinow9595@gmail.com>
2022-02-14 17:09:30 +0900

    레포지토리 정리

--- events.js
+++ events.js
@@ -1,14 +1,17 @@
-// console.log(null === "");
-// console.log(undefined === "");
-// console.log(null === undefined);
-// console.log(null === null);
+// null, undefined
+console.log(null === "");
+console.log(undefined === "");
+console.log(null === undefined);
+console.log(null === null);
 
+// 배열
 const array = ["1", 2, 32, "fas"];
 
 array.push("last");
 
 console.log(array);
 
+// 객체
 const me = {
     lastName: "ji",
     firstName: "now"
@@ -22,6 +25,7 @@ me.blog = "jinow9595.tistory.com";
 
 console.log(me);
 
+// 이벤트 핸들러
 const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 const title = document.querySelector("h1");
 const body = document.querySelector("body");
@@ -85,3 +89,52 @@ function innerWidthChangeEvent() {
         body.classList.remove("middleInnerWidth");
     }
 }
+
+// var, let, const - scope
+var variable = 1;
+let variableLet = 2;
+const variableConst = 3;
+
+if (true) {
+    var variable = 10;
+
+    console.log(`var: ${variable}`); // var: 10
+}
+
+if (true) {
+    let variableLet = 20;
+
+    console.log(`let: ${variableLet}`); // var: 20
+}
+
+if (true) {
+    const variableConst = 30;
+
+    console.log(`const: ${variableConst}`); // const: 30
+}
+
+variableChange();
+
+function variableChange() {
+    var variable = 100;
+
+    console.log(`var : ${variable}`); // var: 100
+}
+
+console.log(`var : ${variable}`); // var: 10
+console.log(`let : ${variableLet}`); // let: 2
+console.log(`const : ${variableConst}`); // const: 3
+
+const array = ["var", "let", "const"];
+// array = "array"; // TypeError: Attempted to assign to readonly property.
+array.push("can push");
+
+console.log(array); // ["var", "let", "const", "can push"]
+
+const object = {
+    name: "object"
+}
+
+object.property = "can add";
+
+console.log(object); // {name: "object", property: "can add"}
